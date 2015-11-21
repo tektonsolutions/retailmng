022072a2aa3b671952c9bfd031990e403f327a43
Jan <jan.mudzki@gmail.com>
2015-11-21 11:15:47 +0800

    init

---
+++ accounts/registration/registration.js
@@ -0,0 +1,11 @@
+if(Meteor.isClient){
+  Template.register.events({
+    'submit form': function(event){
+       event.preventDefault();
+    }
+  });
+
+  Template.register.onRendered(function(){
+
+  });
+}
