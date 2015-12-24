7ddd17b774a37602f12add1fd2ccf4a1877a349b
Jan <jan.mudzki@gmail.com>
2015-12-24 15:11:47 +0800

    fixes and unit CRUD
    refactored default validation rules to one file
    
    added create to units

--- employees/employees.js
+++ employees/employees.js
@@ -132,63 +132,6 @@ if(Meteor.isClient){
        event.preventDefault();
     }
   });
-
-  $.validator.setDefaults({
-    ignore: [],
-    errorClass: 'invalid',
-    errorPlacement: function (error, element) {
-      var select = $("select").get(0);
-      if(select === element.get(0)){
-        $(element)
-        .closest("form")
-        .find("p[class='selectErrorContainer']")
-        .text(error.text());
-      }else {
-        $(element)
-        .closest("form")
-        .find("label[for='" + element.attr("id") + "']")
-        .attr('data-error', error.text());
-      }
-     },
-     rules:{
-      username:{
-        required: true,
-        minlength: 5
-      },
-      password:{
-        required: true,
-        minlength: 5
-      },
-      confirm_password:{
-        equalTo: "#emp_new_pw"
-      },
-      name:{
-        required: true
-      },
-      role: {
-        required: true
-      }
-    },
-    messages:{
-      username:{
-        required: "You must enter a username.",
-        minlength: "Username must be at least {0} characters."
-      },
-      password:{
-        required: "You must enter a password.",
-        minlength: "Password must be at least {0} characters."
-      },
-      confirm_password:{
-        equalTo: "Passwords don't match."
-      },
-      name:{
-        required: "You must enter a name."
-      },
-      role:{
-        required: "Please select a position."
-      }
-    }
-  });
 }
 
 function getFields(){
